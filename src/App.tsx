17c32bde4d1bc2b786d3b4838977b218bb48dee6
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-01 12:08:02 +0200

    Added functionality to use local storage and with single source of truth

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TodoBanner from "./components/TodoBanner";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
@@ -6,14 +6,26 @@ import { TodoContext } from "./contexts/TodoContext";
 import { TodoTypes } from "./types/TodoTypes";
 
 export default function App() {
-  const [allTodos, setAllTodos] = useState<TodoTypes[]>([])
+  const local = localStorage.getItem("allTodos");
+  let data: TodoTypes[] = [];
+
+  if (local) {
+    data = JSON.parse(local);
+  }
+
+  const [allTodos, setAllTodos] = useState<TodoTypes[]>(data);
+
+  useEffect(() => {
+    localStorage.setItem("allTodos", JSON.stringify(allTodos));
+  }, [allTodos]);
+
   return (
     <main className="flex flex-col items-center justify-center max-w-3xl mx-auto py-16 px-8 mt-8 border-slate-300 border">
-      <TodoContext.Provider value={{allTodos, setAllTodos}}>
+      <TodoContext.Provider value={{ allTodos, setAllTodos }}>
         <TodoBanner />
         <TodoForm />
         <TodoList />
       </TodoContext.Provider>
     </main>
-  )
+  );
 }
