90ae6a67aa10c8a56a867dbc1abf5a5fb72937a2
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-01 09:37:36 +0200

    Created Banner Component

---
+++ src/components/TodoBanner.tsx
@@ -0,0 +1,8 @@
+
+export default function TodoBanner() {
+  return (
+    <div className="flex items-center justify-center py-28">
+        <h1 className="text-9xl font-semibold text-gray-200">todos</h1>
+    </div>
+  )
+}
---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,6 @@
+
+export default function TodoForm() {
+  return (
+    <div>TodoForm</div>
+  )
+}
