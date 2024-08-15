c4e5c921b4f4af0d2c584f8e8750049f2c6ee768
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-15 13:41:57 +0200

    Now I deleted everything that had something to do with context API

--- src/contexts/TodoContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import { TodoTypes } from "../types/TodoTypes";
-
-export interface ContextTypes {
-    allTodos: TodoTypes[];
-    setAllTodos: React.Dispatch<React.SetStateAction<TodoTypes[]>>;
-}
-
-export const TodoContext = createContext<ContextTypes | undefined>(undefined)
