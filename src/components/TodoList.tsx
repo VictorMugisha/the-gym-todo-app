6e8fdb8c41b38401dfcde3df836728bb2f4c4aaa
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-15 13:26:46 +0200

    Successfully managed to consume the store's slice in the TodoList Component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,12 +1,15 @@
 import TodoItem from "./TodoItem";
-import { useTodoContext } from "../hooks/useTodoContext";
+// import { useTodoContext } from "../hooks/useTodoContext";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
 
 export default function TodoList() {
-    const { allTodos } = useTodoContext()
+    const todos = useSelector((state: RootState) => state.todos.value)
+    // const { allTodos } = useTodoContext()
     return (
         <div className="w-full mt-4 flex flex-col gap-1">
             {
-                allTodos.map(todo => (
+                todos.map(todo => (
                     <TodoItem key={todo.todoId} todo={todo} />
                 ))
             }
